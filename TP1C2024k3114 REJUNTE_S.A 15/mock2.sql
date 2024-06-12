285dac5f29eb747436ff9a7409f3706cdfd789bd
Naicon67 <cerezotomas67@gmail.com>
2024-06-11 22:27:07 -0300

    wtf

--- TP1C2024k3114 REJUNTE_S.A 15/mock2.sql
+++ TP1C2024k3114 REJUNTE_S.A 15/mock2.sql
@@ -33,28 +33,23 @@ ENVIO_COSTO as total_costo_envios
 */
 
 SELECT
-    TICKET_NUMERO                as nro_ticket,
-    MAX(CAJA_NUMERO)             as nro_caja,
-    MAX(s.id_sucursal)           as id_sucursal,
-    MAX(e.legajo_empleado)       as legajo_empleado,
-    MAX(mp.id_medio_pago)        as id_medio_pago,
-    MAX(dp.id_detalle_pago)      as id_detalle_pago,
-    MAX(PAGO_FECHA)              as fecha_pago,
-    MAX(PAGO_IMPORTE)            as importe,
-    MAX(PAGO_DESCUENTO_APLICADO) as descuento_aplicado
-FROM [GD1C2024].[gd_esquema].[Maestra]
-
-JOIN REJUNTESA.supermercado sup ON SUPER_NOMBRE = sup.nombre
-JOIN REJUNTESA.sucursal s ON SUCURSAL_NOMBRE = s.nombre AND SUPER_NOMBRE = sup.nombre
-LEFT OUTER JOIN REJUNTESA.empleado e ON e.dni = EMPLEADO_DNI
-LEFT OUTER JOIN [GD1C2024].REJUNTESA.medio_pago mp ON mp.nombre = PAGO_MEDIO_PAGO
-LEFT OUTER JOIN REJUNTESA.detalle_pago dp ON
-    dp.nro_tarjeta = PAGO_TARJETA_NRO AND
-    dp.cuotas = PAGO_TARJETA_CUOTAS AND
-    dp.vencimiento_tarjeta = PAGO_TARJETA_FECHA_VENC
-
-WHERE (PAGO_IMPORTE is not null OR CAJA_NUMERO is not null)
-
-GROUP BY TICKET_NUMERO, TICKET_TOTAL_TICKET
-
-ORDER BY TICKET_NUMERO
+    nro_ticket,
+    MIN(fecha_p) as fecha,
+    tipo_comprobante,
+    SUM(sub_total_p) as sub_total,
+    SUM(descuento_promociones_p) as descuento_promociones,
+    SUM(descuento_medio_p) as descuento_medio,
+    SUM(total_p) as total,
+    SUM(total_costo_envios_p) as total_costo_envios
+FROM (SELECT
+        TICKET_NUMERO                           as nro_ticket,
+        MIN(TICKET_FECHA_HORA)                  as fecha_p,
+        MAX(TICKET_TIPO_COMPROBANTE)            as tipo_comprobante,
+        MAX(TICKET_SUBTOTAL_PRODUCTOS)          as sub_total_p,
+        MAX(TICKET_TOTAL_DESCUENTO_APLICADO)    as descuento_promociones_p,
+        MAX(TICKET_TOTAL_DESCUENTO_APLICADO_MP) as descuento_medio_p,
+        MAX(TICKET_TOTAL_TICKET)                as total_p,
+        MAX(TICKET_TOTAL_ENVIO) as total_costo_envios_p
+    FROM [GD1C2024].[gd_esquema].[Maestra] as p
+    GROUP BY TICKET_NUMERO, TICKET_TOTAL_TICKET, TICKET_TOTAL_ENVIO) as v
+GROUP BY nro_ticket, tipo_comprobante
